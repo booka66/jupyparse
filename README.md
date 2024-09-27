8f839fc0379d717261f7a05a551b6d442f203d53
booka66 <jacobbcahoon@gmail.com>
2024-09-27 11:29:29 -0600

    second commit lol

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
+# It's a todo list... that's about it
+
+<!--toc:start-->
+
+- [It's a todo list... that's about it](#its-a-todo-list-thats-about-it)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Todo (lol)](#todo-lol)
+  <!--toc:end-->
+
+## Installation
+
+Just go ahead and use any plugin manager you like, for example with LazyVim:
+
+```lua
+return {
+  {
+    "booka66/nvim_todo",
+  },
+}
+```
+
+You're gonna need Neorg for this to work (or at least look good), so make sure you have that installed. The plugin will write a ~/.todo.json file to store your todo items. The popup is made by parsing this file, so if you delete it, you'll lose your todo list. The popup is rendered using Neorg's stuff, so that's why you'll need it to look good.
+
+## Usage
+
+Example usage:
+![Example Image](./example.png)
+Creating a new todo item:
+![New Item Image](./new_item.png)
+
+- \<leader\>tn - New todo item
+- \<leader\>to - Open todo list
+  - esc or q: close the popup
+  - enter: toggle item as done
+  - i: toggle item as important
+  - p: toggle item as pending
+  - e: edit item (or category)
+  - d: delete item (or category including all items)
+
+## Todo (lol)
+
+- Clear all done items after 5 (?) days of completion
+- Associate items with the files they were created in (hit l to jump to the file)
 # jupyparse
---
+++ lua/jupyparse.lua
@@ -0,0 +1,60 @@
+-- notebook.lua
+
+local M = {}
+
+-- Function to parse the Python file and extract cell information
+local function parse_notebook(file_path)
+	local cells = {}
+	local current_cell = { content = {}, type = "code" }
+
+	for line in io.lines(file_path) do
+		if line:match("^# %%") then
+			-- New cell marker
+			if #current_cell.content > 0 then
+				table.insert(cells, current_cell)
+				current_cell = { content = {}, type = "code" }
+			end
+
+			if line:match("^# %% %[markdown%]") then
+				current_cell.type = "markdown"
+			end
+		else
+			table.insert(current_cell.content, line)
+		end
+	end
+
+	if #current_cell.content > 0 then
+		table.insert(cells, current_cell)
+	end
+
+	return cells
+end
+
+-- Function to render the notebook in a new buffer
+local function render_notebook(cells)
+	vim.cmd("new")
+	local buf = vim.api.nvim_get_current_buf()
+
+	for i, cell in ipairs(cells) do
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "--- Cell " .. i .. " (" .. cell.type .. ") ---" })
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, cell.content)
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "" })
+	end
+
+	vim.bo[buf].modifiable = false
+	vim.bo[buf].buftype = "nofile"
+	vim.bo[buf].filetype = "notebook"
+end
+
+-- Function to open and render a notebook file
+function M.open_notebook(file_path)
+	local cells = parse_notebook(file_path)
+	render_notebook(cells)
+end
+
+-- Command to open a notebook file
+vim.api.nvim_create_user_command("OpenNotebook", function(opts)
+	M.open_notebook(opts.args)
+end, { nargs = 1, complete = "file" })
+
+return M
