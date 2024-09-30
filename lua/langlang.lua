76ffb776157f0e432e3d03c83b102927e81e42fd
booka66 <jacobbcahoon@gmail.com>
2024-09-29 18:41:43 -0600

    updated

--- lua/langlang.lua
+++ lua/langlang.lua
@@ -63,9 +63,11 @@ function M.highlight_error(ns_id, error)
 	-- Highlight the error
 	vim.api.nvim_buf_add_highlight(0, ns_id, "Error", row, col_start, col_end)
 
-	-- Store error information for popup
+	-- Store error information for hover
 	vim.api.nvim_buf_set_extmark(0, ns_id, row, col_start, {
 		end_col = col_end,
+		hl_group = "Error",
+		ephemeral = true,
 		data = { message = error.message, suggestion = error.suggestion },
 	})
 end
@@ -73,46 +75,20 @@ end
 -- Function to show popup on hover
 function M.show_popup()
 	local pos = vim.api.nvim_win_get_cursor(0)
-	local row = pos[1] - 1
-	local col = pos[2]
-
+	local row, col = pos[1] - 1, pos[2]
 	local ns_id = vim.api.nvim_create_namespace("languagetool")
+
 	local extmarks = vim.api.nvim_buf_get_extmarks(0, ns_id, { row, 0 }, { row, -1 }, { details = true })
 
 	for _, extmark in ipairs(extmarks) do
-		local mark_row, mark_col, mark_end_col = extmark[2], extmark[3], extmark[4].end_col
-		if col >= mark_col and col < mark_end_col then
+		local mark_row, mark_col, mark_end_col = unpack(extmark[2])
+		if row == mark_row and col >= mark_col and col < mark_end_col then
 			local data = extmark[4].data
 			local popup_text = string.format("Error: %s\nSuggestion: %s", data.message, data.suggestion)
-
-			-- Create popup
-			local buf = vim.api.nvim_create_buf(false, true)
-			vim.api.nvim_buf_set_lines(buf, 0, -1, true, vim.split(popup_text, "\n"))
-
-			local opts = {
-				relative = "win",
-				width = 60,
-				height = 3,
-				col = mark_col,
-				row = row + 1,
-				anchor = "NW",
-				style = "minimal",
+			vim.lsp.util.open_floating_preview({ popup_text }, "plaintext", {
 				border = "rounded",
-			}
-
-			local win = vim.api.nvim_open_win(buf, false, opts)
-
-			-- Close popup when cursor moves
-			local group = vim.api.nvim_create_augroup("LanguageToolPopup", { clear = true })
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				group = group,
-				callback = function()
-					if vim.api.nvim_win_is_valid(win) then
-						vim.api.nvim_win_close(win, true)
-					end
-				end,
+				focusable = false,
 			})
-
 			break
 		end
 	end
@@ -121,11 +97,11 @@ end
 -- Command to run LanguageTool
 vim.api.nvim_create_user_command("LanguageTool", M.run_languagetool, {})
 
--- Set up autocommand for showing popup on hover
+-- Set up hover functionality
 vim.cmd([[
   augroup LanguageTool
     autocmd!
-    autocmd CursorHold * lua require('languagetool').show_popup()
+    autocmd CursorHold * lua require('langlang').show_popup()
   augroup END
 ]])
 
