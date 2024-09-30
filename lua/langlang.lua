4e4cb781c7f09a9203e5e622e2a77795d3b34840
booka66 <jacobbcahoon@gmail.com>
2024-09-29 18:33:22 -0600

    better parser

--- lua/langlang.lua
+++ lua/langlang.lua
@@ -18,35 +18,33 @@ function M.parse_and_highlight(output)
 	vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
 
 	local current_error = {}
-	for line in output:gmatch("[^\r\n]+") do
-		local line_num, col, rule_id = line:match("(%d+)%.) Line (%d+), column (%d+), Rule ID: (.+)")
-		local message = line:match("Message: (.+)")
-		local suggestion = line:match("Suggestion: (.+)")
-		local context = line:match("%.%.%.(.-)%.%.%.")
+	local in_error = false
 
-		if line_num and col then
+	for line in output:gmatch("[^\r\n]+") do
+		if line:match("^%d+%.%)") then
 			-- New error found, process the previous one if it exists
 			if current_error.row then
 				M.highlight_error(ns_id, current_error)
 			end
+
+			local line_num, col = line:match("Line (%d+), column (%d+)")
 			current_error = {
 				row = tonumber(line_num) - 1,
 				col = tonumber(col) - 1,
-				rule_id = rule_id,
 				message = "",
 				suggestion = "",
 				context = "",
 			}
-		end
-
-		if message then
-			current_error.message = message
-		end
-		if suggestion then
-			current_error.suggestion = suggestion
-		end
-		if context then
-			current_error.context = context
+			in_error = true
+		elseif in_error then
+			if line:match("^Message:") then
+				current_error.message = line:match("^Message: (.+)")
+			elseif line:match("^Suggestion:") then
+				current_error.suggestion = line:match("^Suggestion: (.+)")
+			elseif line:match("^%.%.%.") then
+				current_error.context = line:match("%.%.%.(.-%.%.%.)")
+				in_error = false
+			end
 		end
 	end
 
